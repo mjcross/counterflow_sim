adb1aa8dd37520c5f5d1c5ceeb7cda0079041d2c
Martin Crossley <dev@crossleys.biz>
2025-06-18 10:41:58 +0100

    add option to save animation

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import numpy as np
 from time import sleep
 
 L = 190         # length of CFC in cm
-Fa = 2 / 60     # wort flow in litres/sec
-Fb = 2.5 / 60     # coolant flow in litres/sec
+Fa = 2.0 / 60   # wort flow in litres/sec
+Fb = 3.0 / 60   # coolant flow in litres/sec
 Ta0 = 80.0      # wort input temp
 Tb0 = 14.0      # coolant input temp
 
-N = 1000        # number of simulation points
+N = 254         # number of simulation points
 dx = L/(N-1)    # space between points
 
 Ca = Fa         # use flow rate as a proxy for heat capacity
 Cb = Fb
 
-k = .001 * dx   # thermal conductivity
+k = .003 * dx   # thermal conductivity
 
-Ta = np.array(N * [Ta0])    # initialise elements of both circuits
+Ta = np.array(N * [Tb0])    # initialise both circuits to wort temperature
 Tb = np.array(N * [Tb0])
 
 # create chart objects globally so we can easily update them
@@ -25,7 +25,9 @@ fig, ax = plt.subplots()
 plt.grid(axis='y')
 Ta_plot = ax.plot(Ta, '-r', label='Wort')[0]
 Tb_plot = ax.plot(Tb, '-g', label='Coolant')[0]
-ax.set(xlim=[0,N], ylim=[0,100], ylabel='Temperature')
+Tdiff_plot = ax.plot(Ta - Tb, '-b', label='difference')[0]
+ax.set(xlim=[0,N], ylim=[0.0001, 100], ylabel='Temperature')
+#ax.set_yscale('log')
 ax.legend()
 
 def update_temps():
@@ -51,12 +53,20 @@ def update(frame):
     update_temps()
     Ta_plot.set_ydata(Ta)
     Tb_plot.set_ydata(Tb)
-    return (Ta_plot, Tb_plot)
+    Tdiff_plot.set_ydata(Ta-Tb)
+    return (Ta_plot, Tb_plot, Tdiff_plot)
 
 def main():
-    ani = animation.FuncAnimation(fig=fig, func=update, frames=40, interval=0)
+    ani = animation.FuncAnimation(fig=fig, func=update, save_count=1200, interval=10, blit=True)
+    
+    # display the animation on the screen
     plt.show()
 
+    # save the animation to file
+    #ani.save(filename="./asymptotic_to_coolant_temperature.mp4", fps=60)
+
+    print("Ta", Ta[0], Ta[-1])
+    print("Tb", Tb[0], Tb[-1])
 
 if __name__ == '__main__':
     main()
