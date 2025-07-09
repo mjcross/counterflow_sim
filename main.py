b70d28a113a083b6f46aa5ac0803c56699d4f1e8
Martin Crossley <dev@crossleys.biz>
2025-07-09 10:48:49 +0100

    improve variable names

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import matplotlib.animation as animation
 import numpy as np
 
 class Flow:
-    def __init__(self, length, mass, mass_rate, specific_heat_capacity, Tin, flow_direction='R', N:int=256):
+    def __init__(self, length, total_mass, mass_flow_rate, specific_heat_capacity, Tin, flow_direction='R', N:int=256):
         self.N = N
         self.flow_direction = flow_direction
         self.element_length = length / N
-        self.element_mass = mass / N
-        self.mass_rate = mass_rate
-        self.element_heat_cap = specific_heat_capacity * self.element_mass
+        self.element_mass = total_mass / N
+        self.mass_flow_rate = mass_flow_rate
+        self.element_heat_capacity = specific_heat_capacity * self.element_mass
         self.Tin = Tin
         self.T = np.array(N * [Tin], dtype='float')
 
@@ -30,44 +30,45 @@ class Flow:
         return Tout
 
     def __str__(self):
-        strrep = f'N {self.N} dx {self.element_length} dm {self.element_mass} dC {self.element_heat_cap} '
-        strrep += f'mass flow {self.mass_rate} ({self.flow_direction}) Tin {self.Tin} Tout {self.Tout}\n'
+        strrep = f'N {self.N} dx {self.element_length} dm {self.element_mass} dC {self.element_heat_capacity} '
+        strrep += f'mass flow {self.mass_flow_rate} ({self.flow_direction}) Tin {self.Tin} Tout {self.Tout}\n'
         strrep += f'   {self.T}\n'
         return strrep
     
     def timestep(self, dt):
-        weight_Tin = self.mass_rate * dt / self.element_mass
-        weight_Told = 1 - weight_Tin
-        assert weight_Told >= 0, f'dt too high for mass flow rate (max {self.element_mass / self.mass_rate})'
+        Tnew_proportion = self.mass_flow_rate * dt / self.element_mass
+        Told_proportion = 1 - Tnew_proportion
+        if Told_proportion < 0:
+            raise ValueError (f'dt too high for mass flow rate, max {self.element_mass / self.mass_flow_rate})')
         if self.flow_direction == 'R':
             for n in range(self.N-1, -1, -1):
                 if n > 0:
-                    Tin = self.T[n-1]
+                    Tnew = self.T[n-1]
                 else:
-                    Tin = self.Tin
+                    Tnew = self.Tin
                 Told = self.T[n]
-                self.T[n] = Tin * weight_Tin + Told * weight_Told
+                self.T[n] = Tnew * Tnew_proportion + Told * Told_proportion
         elif self.flow_direction == 'L':
             for n in range(self.N):
                 if n < (self.N - 1):
-                    Tin = self.T[n+1]
+                    Tnew = self.T[n+1]
                 else:
-                    Tin = self.Tin
+                    Tnew = self.Tin
                 Told = self.T[n]
-                self.T[n] = Tin * weight_Tin + Told * weight_Told
+                self.T[n] = Tnew * Tnew_proportion + Told * Told_proportion
         else:
                 raise ValueError('unrecognised flow direction')
 
 class CFC:
     def __init__(self, length, kq, num_elements,
-                 mass_a, mass_rate_a, specific_heat_capacity_a, Tin_a,
-                 mass_b, mass_rate_b, specific_heat_capacity_b, Tin_b):
+                 total_mass_a, mass_flow_rate_a, specific_heat_capacity_a, Tin_a,
+                 total_mass_b, mass_flow_rate_b, specific_heat_capacity_b, Tin_b):
         self.t = 0
         self.length = length
         self.kq = kq
         self.N = num_elements
-        self.a = Flow(length, mass_a, mass_rate_a, specific_heat_capacity_a, Tin_a, 'R', num_elements)
-        self.b = Flow(length, mass_b, mass_rate_b, specific_heat_capacity_b, Tin_b, 'L', num_elements)
+        self.a = Flow(length, total_mass_a, mass_flow_rate_a, specific_heat_capacity_a, Tin_a, 'R', num_elements)
+        self.b = Flow(length, total_mass_b, mass_flow_rate_b, specific_heat_capacity_b, Tin_b, 'L', num_elements)
 
     def __str__(self):
         strrep = f'L {self.length} kq {self.kq}\n'
@@ -78,8 +79,8 @@ class CFC:
     def heat_transfer(self, dt):
         for n in range(self.N):
             dQab = self.kq / self.N * (self.a.T[n] - self.b.T[n]) * dt
-            self.a.T[n] -= dQab / self.a.element_heat_cap
-            self.b.T[n] += dQab / self.b.element_heat_cap
+            self.a.T[n] -= dQab / self.a.element_heat_capacity
+            self.b.T[n] += dQab / self.b.element_heat_capacity
     
     def timestep(self, dt):
         self.a.timestep(dt)
@@ -89,14 +90,18 @@ class CFC:
     
 
 class Simulation:
-    def __init__(self, length, kq, num_elements, time_step,
-                 mass_a, mass_rate_a, specific_heat_capacity_a, Tin_a,
-                 mass_b, mass_rate_b, specific_heat_capacity_b, Tin_b):
+    def __init__(self, length, kq, num_elements, mass_new_old_ratio,
+                 total_mass_a, mass_flow_rate_a, specific_heat_capacity_a, Tin_a,
+                 total_mass_b, mass_flow_rate_b, specific_heat_capacity_b, Tin_b):
         self.cfc = CFC(
             length=length, kq=kq, num_elements=num_elements,
-            mass_a=mass_a, mass_rate_a=mass_rate_a, specific_heat_capacity_a=specific_heat_capacity_a, Tin_a=Tin_a,
-            mass_b=mass_b, mass_rate_b=mass_rate_b, specific_heat_capacity_b=specific_heat_capacity_b, Tin_b=Tin_b)
-        self.dt = time_step
+            total_mass_a=total_mass_a, mass_flow_rate_a=mass_flow_rate_a, specific_heat_capacity_a=specific_heat_capacity_a, Tin_a=Tin_a,
+            total_mass_b=total_mass_b, mass_flow_rate_b=mass_flow_rate_b, specific_heat_capacity_b=specific_heat_capacity_b, Tin_b=Tin_b)
+        
+        # calculate the time step to give the required new/old mass ratio for the smallest element
+        dt_a = mass_new_old_ratio * self.cfc.a.element_mass / self.cfc.a.mass_flow_rate
+        dt_b = mass_new_old_ratio * self.cfc.b.element_mass / self.cfc.b.mass_flow_rate
+        self.dt = min(dt_a, dt_b)
 
         # set up the chart
         self.x = np.linspace(0, length, num_elements)
@@ -139,11 +144,12 @@ class Simulation:
         #   Ta = Te + a e^(-kx x)
         #   Tb = Te + b e^(-kx x)
         cfc = self.cfc
+        np.seterr(divide='raise')
         try:
             self.kx = -np.log((cfc.a.Tout - cfc.b.Tin) / (cfc.a.Tin - cfc.b.Tout))
             self.a = (cfc.a.Tout - cfc.a.Tin) * (cfc.a.Tin - cfc.b.Tout) / ((cfc.a.Tout - cfc.a.Tin) + (cfc.b.Tout - cfc.b.Tin))
             self.b = (cfc.b.Tin - cfc.b.Tout) * (cfc.a.Tin - cfc.b.Tout) / ((cfc.a.Tout - cfc.a.Tin) + (cfc.b.Tout - cfc.b.Tin))
-        except ZeroDivisionError:
+        except FloatingPointError:
             self.kx = 0
             self.a = 0
             self.b = 0
@@ -154,9 +160,9 @@ def main():
     np.set_printoptions(precision=4, floatmode='fixed', threshold=10)
 
     #! this is where we set the parameters for the simulation
-    sim = Simulation(length=1, kq=1, time_step=0.0005, num_elements=512,
-                     mass_a=1, mass_rate_a=2, specific_heat_capacity_a=1, Tin_a=80,
-                     mass_b=1, mass_rate_b=0.75, specific_heat_capacity_b=1, Tin_b=20)
+    sim = Simulation(length=1, kq=1, num_elements=2048, mass_new_old_ratio=0.2,
+                     total_mass_a=1, mass_flow_rate_a=2, specific_heat_capacity_a=1, Tin_a=80,
+                     total_mass_b=1, mass_flow_rate_b=0.75, specific_heat_capacity_b=1, Tin_b=20)
     
     ani = animation.FuncAnimation(
         fig=sim.fig,
@@ -194,8 +200,8 @@ def main():
     deltaT_1 = Ta_1 - Tb_1
 
     # heat capacity flow rates (product of mass flow rate and specific heat capacity)
-    Ca = sim.cfc.a.element_heat_cap / sim.cfc.a.element_mass * sim.cfc.a.mass_rate
-    Cb = sim.cfc.b.element_heat_cap / sim.cfc.b.element_mass * sim.cfc.b.mass_rate
+    Ca = sim.cfc.a.element_heat_capacity / sim.cfc.a.element_mass * sim.cfc.a.mass_flow_rate
+    Cb = sim.cfc.b.element_heat_capacity / sim.cfc.b.element_mass * sim.cfc.b.mass_flow_rate
 
     # infer the CFC conductivity from the HC rates, dT/dx and delta T
     kq_a0 = -Ca * dTadx_0 / deltaT_0
