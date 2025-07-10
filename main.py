6823504ac98f4122a4725ca61ece412b7a8cac98
Martin Crossley <dev@crossleys.biz>
2025-07-10 11:51:23 +0100

    calculate kq estimates directly

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Flow:
         self.element_length = length / N
         self.element_mass = total_mass / N
         self.mass_flow_rate = mass_flow_rate
+        self.specific_heat_capacity = specific_heat_capacity
         self.element_heat_capacity = specific_heat_capacity * self.element_mass
         self.Tin = Tin
         self.T = np.array(N * [Tin], dtype='float')
@@ -69,6 +70,7 @@ class CFC:
         self.N = num_elements
         self.a = Flow(length, total_mass_a, mass_flow_rate_a, specific_heat_capacity_a, Tin_a, 'R', num_elements)
         self.b = Flow(length, total_mass_b, mass_flow_rate_b, specific_heat_capacity_b, Tin_b, 'L', num_elements)
+        self.element_kq = kq * self.a.element_length
 
     def __str__(self):
         strrep = f'L {self.length} kq {self.kq}\n'
@@ -78,7 +80,7 @@ class CFC:
     
     def heat_transfer(self, dt):
         for n in range(self.N):
-            dQab = self.kq / self.N * (self.a.T[n] - self.b.T[n]) * dt
+            dQab = self.element_kq * (self.a.T[n] - self.b.T[n]) * dt
             self.a.T[n] -= dQab / self.a.element_heat_capacity
             self.b.T[n] += dQab / self.b.element_heat_capacity
     
@@ -118,8 +120,24 @@ class Simulation:
 
     def __str__(self):
         strrep = f'Counterflow: dt {self.dt} {self.cfc}\n'
-        strrep += f'Model: a {self.a:.4f} b {self.b:.4f} xi {1.0/self.kx:.4f} Te {self.Te:.4f}\n'
+        strrep += f'Model: a {self.a:.4f} b {self.b:.4f} xi {self.xi:.4f} Te {self.Te:.4f}\n'
         return strrep
+
+    def fit_model(self):
+        # fit exponential models to input and output temps, so that
+        #   Ta = Te + a e^(-x/xi)
+        #   Tb = Te + b e^(-x/xi)
+        cfc = self.cfc
+        np.seterr(divide='raise')
+        try:
+            self.xi = -self.cfc.length / np.log((cfc.a.Tout - cfc.b.Tin) / (cfc.a.Tin - cfc.b.Tout))
+            self.a = (cfc.a.Tout - cfc.a.Tin) * (cfc.a.Tin - cfc.b.Tout) / ((cfc.a.Tout - cfc.a.Tin) + (cfc.b.Tout - cfc.b.Tin))
+            self.b = (cfc.b.Tin - cfc.b.Tout) * (cfc.a.Tin - cfc.b.Tout) / ((cfc.a.Tout - cfc.a.Tin) + (cfc.b.Tout - cfc.b.Tin))
+        except FloatingPointError:
+            self.xi = 0
+            self.a = 0
+            self.b = 0
+        self.Te = cfc.a.Tin - self.a
 
     def update(self, frame):
         self.cfc.timestep(self.dt)
@@ -128,41 +146,24 @@ class Simulation:
         self.Tdiff_plot.set_ydata(self.cfc.a.T - self.cfc.b.T)
 
         self.fit_model()
-        model_x = np.linspace(0, 1, 6)
-        model_Ta = self.Te + self.a * np.exp(-self.kx * model_x)
-        model_Tb = self.Te + self.b * np.exp(-self.kx * model_x)
+        model_x = np.linspace(0, self.cfc.length, 6)
+        model_Ta = self.Te + self.a * np.exp(-model_x/self.xi)
+        model_Tb = self.Te + self.b * np.exp(-model_x/self.xi)
         self.Ta_model_plot.set_xdata(model_x)
         self.Ta_model_plot.set_ydata(model_Ta)
         self.Tb_model_plot.set_xdata(model_x)
         self.Tb_model_plot.set_ydata(model_Tb)
 
-
         return (self.Ta_plot, self.Tb_plot, self.Tdiff_plot, self.Ta_model_plot, self.Tb_model_plot)
-    
-    def fit_model(self):
-        # fit exponential models to input and output temps, so that
-        #   Ta = Te + a e^(-kx x)
-        #   Tb = Te + b e^(-kx x)
-        cfc = self.cfc
-        np.seterr(divide='raise')
-        try:
-            self.kx = -np.log((cfc.a.Tout - cfc.b.Tin) / (cfc.a.Tin - cfc.b.Tout))
-            self.a = (cfc.a.Tout - cfc.a.Tin) * (cfc.a.Tin - cfc.b.Tout) / ((cfc.a.Tout - cfc.a.Tin) + (cfc.b.Tout - cfc.b.Tin))
-            self.b = (cfc.b.Tin - cfc.b.Tout) * (cfc.a.Tin - cfc.b.Tout) / ((cfc.a.Tout - cfc.a.Tin) + (cfc.b.Tout - cfc.b.Tin))
-        except FloatingPointError:
-            self.kx = 0
-            self.a = 0
-            self.b = 0
-        self.Te = cfc.a.Tin - self.a
 
         
 def main():
     np.set_printoptions(precision=4, floatmode='fixed', threshold=10)
 
     #! this is where we set the parameters for the simulation
-    sim = Simulation(length=1, kq=1, num_elements=2048, mass_new_old_ratio=0.2,
-                     total_mass_a=1, mass_flow_rate_a=2, specific_heat_capacity_a=1, Tin_a=80,
-                     total_mass_b=1, mass_flow_rate_b=0.75, specific_heat_capacity_b=1, Tin_b=20)
+    sim = Simulation(length=2, kq=0.5, num_elements=256, mass_new_old_ratio=0.2,
+                     total_mass_a=1, mass_flow_rate_a=0.75, specific_heat_capacity_a=1, Tin_a=80,
+                     total_mass_b=2, mass_flow_rate_b=0.25, specific_heat_capacity_b=1, Tin_b=20)
     
     ani = animation.FuncAnimation(
         fig=sim.fig,
@@ -179,15 +180,16 @@ def main():
     # fitted model parameters
     a = sim.a
     b = sim.b
-    kx = sim.kx
+    xi = sim.xi
+    L = sim.cfc.length
 
     # derivatives of fitted exponentials 
     #   dTa/dx = (-a kx) e^(-kx x)
     #   dTb/dx = (-b kx) e^(-kx x)
-    dTadx_0 = -a * kx
-    dTadx_1 = -a * kx * np.exp(-kx)
-    dTbdx_0 = -b * kx
-    dTbdx_1 = -b * kx * np.exp(-kx)
+    dTadx_0 = -a / xi
+    dTadx_1 = -a / xi * np.exp(-L/xi)
+    dTbdx_0 = -b / xi
+    dTbdx_1 = -b / xi * np.exp(-L/xi)
 
     # temps at either end of the CFC
     Ta_0 = sim.cfc.a.Tin
@@ -195,33 +197,22 @@ def main():
     Tb_0 = sim.cfc.b.Tout
     Tb_1 = sim.cfc.b.Tin
 
-    # temp differences at either end of the CFC
-    deltaT_0 = Ta_0 - Tb_0
-    deltaT_1 = Ta_1 - Tb_1
-
     # heat capacity flow rates (product of mass flow rate and specific heat capacity)
-    Ca = sim.cfc.a.element_heat_capacity / sim.cfc.a.element_mass * sim.cfc.a.mass_flow_rate
-    Cb = sim.cfc.b.element_heat_capacity / sim.cfc.b.element_mass * sim.cfc.b.mass_flow_rate
+    Ca_rate = sim.cfc.a.specific_heat_capacity * sim.cfc.a.mass_flow_rate
+    Cb_rate = sim.cfc.b.specific_heat_capacity * sim.cfc.b.mass_flow_rate
 
-    # infer the CFC conductivity from the HC rates, dT/dx and delta T
-    kq_a0 = -Ca * dTadx_0 / deltaT_0
-    kq_a1 = -Ca * dTadx_1 / deltaT_1
-    kq_b0 = -Cb * dTbdx_0 / deltaT_0
-    kq_b1 = -Cb * dTbdx_1 / deltaT_1
+    kq_a = 1/L * Ca_rate * (Ta_1-Ta_0)/(Ta_1-Ta_0+Tb_0-Tb_1) * np.log((Ta_0-Tb_0)/(Ta_1-Tb_1))
+    kq_b = 1/L * Cb_rate * (Tb_1-Tb_0)/(Ta_1-Ta_0+Tb_0-Tb_1) * np.log((Ta_0-Tb_0)/(Ta_1-Tb_1))
 
-    calc_kq = (kq_a0 + kq_a1 + kq_b0 + kq_b1) / 4.0
+    kq_mean = (kq_a + kq_b) / 2
 
     print('Calculated kq')
-    print(f'dTa/dx {dTadx_0:.4f}, {dTadx_1:.4f}')
-    print(f'dTb/dx {dTbdx_0:.4f}, {dTbdx_1:.4f}')
-    print(f'deltaT {deltaT_0:.4f}, {deltaT_1:.4f}')
-    print(f'kq_a0 {kq_a0:.4f}\nkq_a1 {kq_a1:.4f}\nkq_b0 {kq_b0:.4f}\nkq_b1 {kq_b1:.4f}')
-    print(f'kq_mean {calc_kq:.4f}')
+    print(f'kq_a {kq_a:.4f}\nkq_b {kq_b:.4f}\nkq_mean {kq_mean:.4f}')
 
      # calculate the spatial distibution parameters
-    calc_xi = -1/(calc_kq * (Ca - Cb)/(Ca * Cb))
-    calc_a = 1/(1 - Ca/Cb * np.exp(-1/calc_xi)) * (Ta_0 - Tb_1)
-    calc_b = 1/(Cb/Ca - np.exp(-1/calc_xi)) * (Ta_0 - Tb_1)
+    calc_xi = -1/(kq_mean * (Ca_rate - Cb_rate)/(Ca_rate * Cb_rate))
+    calc_a = 1/(1 - Ca_rate/Cb_rate * np.exp(-L/calc_xi)) * (Ta_0 - Tb_1)
+    calc_b = 1/(Cb_rate/Ca_rate - np.exp(-L/calc_xi)) * (Ta_0 - Tb_1)
     calc_Te = Ta_0 - calc_a
 
     print('\nCalculated spatial parmeters')
